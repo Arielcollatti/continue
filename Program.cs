b7e72fbe20f7439e72a61716c0d76d1e959d0954
arielcollatti <colatti2017@gmail.com>
2023-03-09 11:53:14 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("continue");
+Console.WriteLine();
+
+Console.Write("pressione uma tecla pra continuar...");
+Console.ReadKey();
+Console.WriteLine();
+
+Console.WriteLine("muito obrigado por abrir este programa");
+
+
